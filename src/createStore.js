0795456f792ecad726470722468b0957b8cb1c28
Jennifer Volpe <jennifervolpe@Jennifers-MacBook-Pro.local>
2017-10-27 15:51:08 -0400

    Done.

--- src/createStore.js
+++ src/createStore.js
@@ -1,1 +1,20 @@
 
+export default function createStore(reducer) {
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action)
+    console.log(`the state is ${state.count}`)
+    console.log(`the action is ${action.type}`)
+    // render()
+  };
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+  import ReactDOM from 'react-dom';
+  import App from './App';
+  import changeCount from './reducers/changeCount';
+  import createStore from './createStore';
 
-export function render() {
-  ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-  );
-};
+  const store = createStore(changeCount);
 
-render();
+  export function render() {
+    ReactDOM.render(
+      <App store={store} />,
+      document.getElementById('root')
+    );
+  };
+
+  render();
